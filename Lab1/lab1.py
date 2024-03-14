ec55e3dfc8ee19120a487307bdf100fcef9f72c9
Alessandro Saffiotti <asaffio@aass.oru.se>
2024-03-14 01:54:55 +0100

    Removed rosnode creation

--- Lab1/lab1.py
+++ Lab1/lab1.py
@@ -1,9 +1,7 @@
 #!/usr/bin/env python3
-import rospy
 import toplevel
 
 
 if __name__ == '__main__':
-    rospy.init_node('airob_lab1', anonymous=False)
     toplevel = toplevel.TopLevelLoop(goal = (5.0, 1.0, ), debug = 1)
     toplevel.run()
